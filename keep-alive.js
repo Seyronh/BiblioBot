23c90d3c0034b14d22880281073d818584a9d06e
Seyronh <seyronh@hotmail.com>
2025-01-02 15:01:43 +0100

    keep-alive

---
+++ keep-alive.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+http
+	.createServer(function (req, res) {
+		res.write("I'm Alive"); // Escribiendo en la respuesta
+		res.end("Hello World!"); // Enviando la respuesta y finalizando
+	})
+	.listen(8080);
